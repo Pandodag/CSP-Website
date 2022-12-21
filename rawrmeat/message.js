e4a77aa862c63466b9b721ad69ed4ccca1020ae1
Pandodag <69488380+Pandodag@users.noreply.github.com>
2022-12-20 20:21:07 -0800

    Delete rawrmeat directory

--- rawrmeat/message.js
+++
@@ -1,3 +0,0 @@
-function msg(){  
- alert("Hello consumer, I was alive two minutes ago, frolicking on the pleasant grassy fields while the sun glistened on my skin, when everything I ever had and dreamed of was taken for me, all just to be served on this plate.");  
-}  
